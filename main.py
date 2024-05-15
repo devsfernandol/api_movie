a29cb12ea1bb728e5cdf1b43e30852c540ab5e03
devsfernandol <devsfernandol@gmail.com>
2024-05-15 16:47:58 -0400

    Primer metodo post Creacion de peliculas

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
 
 app=FastAPI()
@@ -58,3 +58,20 @@ def get_movie(id:int):
 
 def get_movies_category(category:str, year:int):
     return [item for item in movies if item['category'] == category ]
+
+
+@app.post('/movies/', tags=['Movies'])
+
+def create_movies(id: int = Body(), title: str = Body(), overview:str = Body(), year:int = Body(), rating: float = Body(), category: str = Body()):
+
+    movies.append(
+        {"id":id,
+         "title":title,
+         "overview":overview,
+         "year":year,
+         "rating":rating,
+         "category":category
+         }
+    )
+
+    return movies
