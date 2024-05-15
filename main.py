b9d14ecdf2ec0d30a6fbe7e8812ff47194e0cc82
devsfernandol <devsfernandol@gmail.com>
2024-05-15 15:50:05 -0400

    Creando la ruta movies_get

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app=FastAPI()
+app.title="FastAPI Para Peliculas"
+app.version="0.0"
 
-@app.get('/')
+
+movies =[
+    {
+
+        "id":1,
+        "title":"Avatar",
+        "overview":"En mundo Extrano",
+        "year":"2009",
+        "ranting":1.7,
+        "category":"Ficcion"
+        
+    }
+]
+
+
+
+@app.get('/', tags=['Home'])
 
 def message():
 
     return "Hello world"
+
+@app.get('/movies', tags=['Movies'])
+
+def get_movies():
+    return movies
